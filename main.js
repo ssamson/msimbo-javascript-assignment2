b39264264083536e0fac3503ef3c206ec223bc08
Stephanie Samson <sasamson98@hotmail.com>
2019-10-16 10:06:29 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+// Write the function printInt(n) taking one parameter n and print all natural numbers from 1 to n in console.
+function printInt(n) {
+    for(var i=1; i<=n; i++) {
+        console.log(i);
+    }
+}
+printInt(100);
